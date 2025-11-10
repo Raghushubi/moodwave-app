26e91c7936b1ffae967d90fb400678761707f39a
greeshmithasai006 <greeshmithasaigutta@gmail.com>
2025-11-11 03:40:43 +0530

    Updated backend code and pages

--- backend/routes/matchRoutes.js
+++ backend/routes/matchRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
 import { findMatches } from "../controllers/matchController.js";
 const router = express.Router();
-router.get(":moodId", findMatches); // Example: /api/match/happyId\nexport default router;
+router.get('/:moodId', findMatches); // Example: /api/match/happyId\nexport default router;
 export default router;
