1ef693a601cb9a320abe453f1318b852a61aa5e5
greeshmithasai006 <greeshmithasaigutta@gmail.com>
2025-11-11 02:55:24 +0530

    Updated backend code and added matching logic

--- backend/routes/matchRoutes.js
+++ backend/routes/matchRoutes.js
@@ -2,3 +2,4 @@ import express from "express";
 import { findMatches } from "../controllers/matchController.js";
 const router = express.Router();
 router.get(":moodId", findMatches); // Example: /api/match/happyId\nexport default router;
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+
 // Import MongoDB connection function (from Raghu’s config)
 import { connectDB } from "./config/db.js";
 
